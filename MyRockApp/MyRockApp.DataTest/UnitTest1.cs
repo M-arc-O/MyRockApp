c94278acb2b7c85cd87eb67f81f6578a2a34b340
Marco Trapman <marco.trapman@capgemini.com>
2022-02-12 12:54:20 +0100

    Intitial commit with basic setup

---
+++ MyRockApp/MyRockApp.DataTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyRockApp.DataTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MyRockApp/MyRockAppTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyRockAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
